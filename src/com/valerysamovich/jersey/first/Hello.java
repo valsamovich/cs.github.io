02f1b0787785e7baa3eea5de315e6d4fbb0230fb
valerysamovich <valerysamovich@gmail.com>
2014-09-03 11:58:26 -0400

    Update

--- src/com/valerysamovich/jersey/first/Hello.java
+++ src/com/valerysamovich/jersey/first/Hello.java
@@ -4,15 +4,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-/**
- * Plain old Java Object it does not extend as class or implements an interface.
- *
- * The class registers its methods for the HTTP GET request using the @GET 
- * annotation. Using the @Produces annotation, it defines that it can deliver 
- * several MIME types, text, XML and HTML.
- *
- * The browser requests per default the HTML MIME type.
- */
+// Java Object it does not extend as class or implements an interface
+
+// The class registers its methods for the HTTP GET request using the @GET annotation. 
+// Using the @Produces annotation, it defines that it can deliver several MIME types,
+// text, XML and HTML. 
+
+// The browser requests per default the HTML MIME type.
 
 public class Hello {
 
