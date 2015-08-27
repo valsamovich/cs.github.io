4286609b3f125fb8d9acd70c9ae7ee8958685c66
Valery Samovich <valerysamovich@gmail.com>
2015-08-27 15:38:59 -0400

    Add shell scripts

---
+++ src/main/shell/math.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Basic arithmetic using let
+
+let a=5+4
+echo $a #9
+
+let "a = 5 + 4"
+echo $a #9
---
+++ src/main/shell/vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Ask the user for their name
+
+echo "Hello, who am I talking to?"
+read varName
+echo "It's nice to meet you" $varName
