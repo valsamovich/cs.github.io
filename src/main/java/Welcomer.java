97729ece27232a089091578102ca7fea3b5edf71
Valery Samovich <valerysamovich@gmail.com>
2015-09-04 13:53:04 -0400

    Create Welcomer.java

---
+++ src/main/java/Welcomer.java
@@ -0,0 +1,13 @@
+// Welcomer class
+public class Welcomer {
+	// Declares the variable and assigning the value
+	// Instance variable of field name "welcome"
+	// Private - it's can be used only inside this class
+	private String welcome = "Hello!";
+	
+	// Create a new method "sayHello"
+	public void sayHello(){
+		// Display the value of the string object/variable "welcome"
+		System.out.println(welcome); 
+	}
+}
