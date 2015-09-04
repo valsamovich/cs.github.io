18f92cd915e882091b3319a8083da9c36840ae9c
Valery Samovich <valerysamovich@gmail.com>
2015-09-04 13:52:35 -0400

    Update and rename Notepad.java to SimpleApplication.java

---
+++ src/main/java/SimpleApplication.java
@@ -0,0 +1,15 @@
+
+// Simple Application App
+// Created July 21th, 2013
+// Created by Valery Y. Samovich
+public class SimpleApplication {
+	
+	// Main method "main"
+	public static void main(String[] args) {
+		// Create of in instance of the class named Welcomer
+		Welcomer welcomer = new Welcomer(); // Object
+		// Call the "sayHello" method by dot "."
+		welcomer.sayHello();
+	}
+
+}
