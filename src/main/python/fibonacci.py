5245f226025e1b1b59812102c268b7f68b58a9dc
Valery Samovich <valerysamovich@gmail.com>
2015-09-23 13:14:15 -0400

    Add fibonacci example

---
+++ src/main/python/fibonacci.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# Declare vars
+a = 0
+b = 1
+
+# Write an initial sub-sequence of the Fibonacci series
+while b < 10:
+    print b
+    a, b = b, a + b
--- src/main/python/lumberjack.py
+++
@@ -1,5 +0,0 @@
-# name = input("What's your name? ")
-# if name == "Kenneth":
-#    print({} " is a lumberjack and he/she's OK!".format(name))
-# else:
-#    print(name + " sleeps all night " + " works all day!")
