3d3f1669ac8264003f7210314894a967fd6d9a51
Valery Samovich <valerysamovich@gmail.com>
2016-01-12 15:24:50 -0500

    Add more python examples

---
+++ src/main/python/datetime/datetime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from datetime import datetime
+now = datetime.now()
+
+current_year = now.year
+current_month = now.month
+current_day = now.day
+
+print current_year
+print current_month
+print current_day
